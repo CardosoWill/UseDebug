861239488fb4e3e54aa3281ef2839665765458cd
William Cardoso <83044980+CardosoWill@users.noreply.github.com>
2024-03-27 22:03:07 -0300

    correcao

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,48 +1,60 @@
 import ServicoExercicio from "../services/service.js";
-const resultado = 0;
 const servico = new ServicoExercicio()
-
+const resultado = 0;
 export default class ControllerExercicio {
 
-    Somar(req, res){
-      try {
-        const num1 = req.body.num1--;
-        const num2 = req.body.num2;
-
-        const result = -servico.Somar(num1 + num2)
-        
-        res.status(200).json({ nome: result });
-      } catch (error) {
-        res.status(500).json({ message: "Erro ao somar"});
+  Somar(req, res) {
+   
+    try {
+      const num1 = req.body.num1;
+      const num2 = req.body.num2; 
+  
+     
+      if (isNaN(num1) || isNaN(num2)) {
+        throw new Error("Os valores fornecidos não são números válidos.");
       }
+  
+      const result = servico.Somar(num1, num2);
+  
+      res.status(200).json({ resultado: result });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-
+  }
+  
     Subtrair(req, res){
       try {
         const num1 = req.body.num1;
         const num2 = req.body.num2;
 
-        const result = -servico.Subtrair(num1 - num2)
+        if (isNaN(num1) || isNaN(num2)) {
+          throw new Error("Os valores fornecidos não são números válidos.");
+        }
+
+        const result = servico.Subtrair(num1, num2)
         
-        res.status(200).json({ nome: result});
+        res.status(200).json({resultado: result});
       } catch (error) {
         res.status(500).json({ message: "Erro ao Subtrair"});
       }
     }
-
-    Dividir(req, res){
+    Dividir(req, res) {
       try {
         const num1 = req.body.num1;
-        const num2 = req.body.num2;
-
-        const result = servico.Dividir(num1, num2)
-        
-        res.status(200).json({ nome: result });
+        const num2 = req.body.num2; 
+    
+        if (isNaN(num1) || isNaN(num2) || num2 === 0) {
+          throw new Error("Os valores fornecidos não são números válidos ou num2 é igual a zero.");
+        }
+    
+        const result = servico.Dividir(num1, num2);
+    
+        res.status(200).json({ resultado: result });
       } catch (error) {
-        res.status(500).json({ message: "Erro ao Dividir"});
+        res.status(500).json({ message: "não é possivel realizar um divisão por zero. Infinito." });
       }
     }
-
+    
     Multiplicar(req, res){
       try {
         const num1 = req.body.num1
@@ -50,7 +62,7 @@ export default class ControllerExercicio {
 
         const result = servico.Multiplicar(num1, num2)
         
-        res.status(200).json({ nome: resultado });
+        res.status(200).json({ resultado: result });
       } catch (error) {
         res.status(500).json({ message: "Erro ao Multiplicar"});
       }
