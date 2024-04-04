ff11149ed845cf99af20fdd57daa4bb5a700f611
CardosoWill <wiliamcardoso01032005@gmail.com>
2024-04-03 21:05:21 -0300

    teste

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -44,27 +44,31 @@ export default class ControllerExercicio {
         const num2 = req.body.num2; 
     
         if (isNaN(num1) || isNaN(num2) || num2 === 0) {
-          throw new Error("Os valores fornecidos não são números válidos ou num2 é igual a zero.");
+          throw new Error("Os valores fornecidos não são números válidos");
         }
     
         const result = servico.Dividir(num1, num2);
     
         res.status(200).json({ resultado: result });
       } catch (error) {
-        res.status(500).json({ message: "não é possivel realizar um divisão por zero. Infinito." });
+        res.status(500).json({ message: "não é possivel realizar a divisão" });
       }
     }
     
     Multiplicar(req, res){
       try {
-        const num1 = req.body.num1
-        const num2 = req.body.num2
-
-        const result = servico.Multiplicar(num1, num2)
+        const num1 = req.body.num1;
+        const num2 = req.body.num2;
+    
+        if (isNaN(num1) || isNaN(num2)) {
+          throw new Error("Os valores fornecidos não são números válidos.");
+        }
+    
+        const result = servico.Multiplicar(num1, num2);
         
         res.status(200).json({ resultado: result });
       } catch (error) {
-        res.status(500).json({ message: "Erro ao Multiplicar"});
+        res.status(500).json({ message: "não é possivel realizar Multiplicação"});
       }
     }
 
